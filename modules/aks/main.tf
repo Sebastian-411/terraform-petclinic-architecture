28a45381eaa1267a5e0d9e721d5cea7b7a540496
Juan Sebastian Diaz Sanchez <sg5043161@gmail.com>
2024-11-20 13:23:58 -0500

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,3 +1,19 @@
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_pet.kube_config[0].host
+  cluster_ca_certificate = azurerm_kubernetes_cluster.aks_pet.kube_config[0].cluster_ca_certificate
+  client_key             = azurerm_kubernetes_cluster.aks_pet.kube_config[0].client_key
+  client_certificate     = azurerm_kubernetes_cluster.aks_pet.kube_config[0].client_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks_pet.kube_config[0].host
+    cluster_ca_certificate = azurerm_kubernetes_cluster.aks_pet.kube_config[0].cluster_ca_certificate
+    client_key             = azurerm_kubernetes_cluster.aks_pet.kube_config[0].client_key
+    client_certificate     = azurerm_kubernetes_cluster.aks_pet.kube_config[0].client_certificate
+  }
+}
+
 resource "azurerm_kubernetes_cluster" "aks_pet" {
   name                = var.aks_name
   location            = var.location
@@ -15,4 +31,54 @@ resource "azurerm_kubernetes_cluster" "aks_pet" {
   }
 
   tags = var.tags
+}
+
+resource "helm_release" "linkerd" {
+  name       = "linkerd"
+  repository = "https://helm.linkerd.io/stable"
+  chart      = "linkerd2"
+  namespace  = "linkerd"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values/linkerd-values.yaml")  # Si tienes un archivo de configuración específico para Linkerd
+  ]
+}
+
+# Instalación de KEDA usando Helm
+resource "helm_release" "keda" {
+  name       = "keda"
+  repository = "https://kedacore.github.io/charts"
+  chart      = "keda"
+  namespace  = "keda"
+  create_namespace = true
+}
+
+resource "kubernetes_manifest" "app_scaled_object" {
+  manifest = {
+    apiVersion = "keda.sh/v1alpha1"
+    kind       = "ScaledObject"
+    metadata = {
+      name      = "my-app-scaler"
+      namespace = "default"
+    }
+    spec = {
+      scaleTargetRef = {
+        apiVersion = "apps/v1"
+        kind       = "Deployment"
+        name       = "my-app"
+      }
+      minReplicaCount = 1
+      maxReplicaCount = 5
+      triggers = [
+        {
+          type = "cpu"
+          metadata = {
+            type  = "Utilization"
+            value = "50"
+          }
+        }
+      ]
+    }
+  }
 }
