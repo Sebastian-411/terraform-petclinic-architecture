41e77be32ec168a735f2bc0ba169e13dcd8138ff
Juan Sebastian Diaz Sanchez <sg5043161@gmail.com>
2024-11-20 18:22:33 -0500

    Update main.tf

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_key_vault" "key_vault" {
-  name                        = "petskeyvault-2024-10-10"
+  name                        = "petskeyvault-2024-10101010"
   location                    = var.location
   resource_group_name         = var.resource_group_name
   enabled_for_disk_encryption = var.enabled_for_disk_encryption
